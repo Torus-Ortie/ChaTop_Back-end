4b8d67916880b7837129f82e116cbc8aef562346
Torus-Ortie <velez.romain@orange.fr>
2024-09-13 16:31:03 +0200

    Init dependencies

---
+++ src/main/java/com/example/openclassrooms/chatop_back_end/ChatopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.openclassrooms.chatop_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openclassrooms/chatop_back_end/ChatopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openclassrooms.chatop_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
